3b36a39bb9841623ae871f07c50aeeb12dda70d3
RoeiGida <roeiroei77777@gmail.com>
2021-05-14 18:46:10 +0300

    controller path change

--- controller/backend_controller.js
+++ controller/backend_controller.js
@@ -23,7 +23,8 @@ const express = require('express');
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-if(process.cwd().split('\\')[-1] === 'controller')
+let cwdProc = process.cwd().split('\\');
+if(cwdProc[cwdProc.length-1] === 'controller')
     process.chdir('../');
 
 app.get('/', ((req, res) => {
